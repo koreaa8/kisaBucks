88654f50b8944d05a4607f6418131356ec998489
kmju1997 <kmju1997@gmail.com>
2019-02-19 14:27:48 +0900

    fixed error for server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var request = require('request');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs'));
+app.engine('html', require('ejs').renderFile);
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
